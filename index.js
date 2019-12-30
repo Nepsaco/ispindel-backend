9675e920d447c4e6528c3aeb5521368f631492f3
Nepsaco <tobie.tsuzuki@gmail.com>
2019-12-30 14:12:33 -0700

    ispindel post and get route made

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 9000
+const ispindel = require('./routes/ispindel')
 
 app.listen(port, () => {
     console.log(`listening on port: ${port}`)
 })
+
+app.use(ispindel)
--- routes/ispindel.js
+++ routes/ispindel.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 
+router.get('/ispindel', (request, response) => {
+    response.json({ message: 'hi' })
+})
+
 router.post('/ispindel', (request, response) => {
-    console.log(request.body)
+    response.json({ message: 'hi' })
 })
 
 
-module.exports = route
+
+module.exports = router
