b7a70f49b166d3e314ab8043f0a77d6230636456
Nepsaco <tobie.tsuzuki@gmail.com>
2019-12-30 15:56:54 -0700

    testing ispindel object

--- db/database.js
+++ db/database.js
@@ -1,1 +1,4 @@
-const knex
+const connection = require('../knexfile')[process.env.NODE_ENV || 'development']
+const database = require('knex')(connection)
+
+module.exports = database
--- routes/ispindel.js
+++ routes/ispindel.js
@@ -2,9 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/ispindel', (request, response) => {
-    console.log('request', request)
-    console.log('response', response)
-    response.json({ message: 'hi' })
+    response.json(request)
 })
 
 
