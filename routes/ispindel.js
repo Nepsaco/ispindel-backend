16b66a18b8db8f9d68573e80b320b6643a878395
Nepsaco <tobie.tsuzuki@gmail.com>
2019-12-30 16:46:59 -0700

    testing ispindel object

--- routes/ispindel.js
+++ routes/ispindel.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/ispindel', (request, response) => {
-    response.json(request)
+    console.log('body', request.body)
+    console.log('params', request.params)
+    console.log('combo', request.body.params)
+    console.log('combo', request.params.body)
 })
 
 
