e57755ee2fe869003b1a2fcfa08121d4896384fd
Nepsaco <tobie.tsuzuki@gmail.com>
2019-12-30 14:50:54 -0700

    console logging to see what data i am getting bacck added body parser cors and pg

--- routes/ispindel.js
+++ routes/ispindel.js
@@ -1,12 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-
-router.get('/ispindel', (request, response) => {
-    response.json({ message: 'hi' })
-})
-
 router.post('/ispindel', (request, response) => {
+    console.log(response.body)
     response.json({ message: 'hi' })
 })
 
